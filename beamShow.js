aeec5fa22a8b4e0f7d4d60907154b35915ab77b1
Chris Maloney <cmaloney@euronetworldwide.com>
2018-10-28 09:07:51 -0500

    ...

--- beamShow.js
+++ beamShow.js
@@ -15,53 +15,61 @@ const standardColors = [
   Beam.Color.Lavender
   ];
 
-const halloweenColors = [
-  Beam.Color.White,
-  Beam.Color.Red,
-  Beam.Color.Orange,
-  Beam.Color.Green,
-  Beam.Color.Blue,
-  Beam.Color.Magenta,
-  ];
-  
-const testGobos = [
-  Beam.Gobo.Off,
-  Beam.Gobo.Dot3,
-  Beam.Gobo.Dot6,
-  Beam.Gobo.ThreePoints,
-  Beam.Gobo.FourPoints,
-  Beam.Gobo.Star13,
-  Beam.Gobo.Star15,
-  Beam.Gobo.VerticalBar,
-  Beam.Gobo.HorizontalBar
-];
+  const halloweenColors = [
+    Beam.Color.Magenta,
+    Beam.Color.Red,
+    Beam.Color.Orange,
+    Beam.Color.Blue,
+    Beam.Color.Green,
+    Beam.Color.White,
+    ];
 
-const testStrobes = [
-  Beam.Strobe.Off,
-  Beam.Strobe.Slow,
-  Beam.Strobe.Medium,
-  Beam.Strobe.Fast
-];
+    const darkHalloweenColors = [
+    Beam.Color.Magenta,
+    Beam.Color.Red,
+    Beam.Color.Orange,
+    Beam.Color.Blue,
+    Beam.Color.Green,
+    ];
+        
+// const testGobos = [
+//   Beam.Gobo.Off,
+//   Beam.Gobo.Dot3,
+//   Beam.Gobo.Dot6,
+//   Beam.Gobo.ThreePoints,
+//   Beam.Gobo.FourPoints,
+//   Beam.Gobo.Star13,
+//   Beam.Gobo.Star15,
+//   Beam.Gobo.VerticalBar,
+//   Beam.Gobo.HorizontalBar
+// ];
+
+// const testStrobes = [
+//   Beam.Strobe.Off,
+//   Beam.Strobe.Slow,
+//   Beam.Strobe.Medium,
+//   Beam.Strobe.Fast
+// ];
  
-const testPrisms = [
-  Beam.Prism.Off,
-  Beam.Prism.On
-];
+// const testPrisms = [
+//   Beam.Prism.Off,
+//   Beam.Prism.On
+// ];
  
-const testPrismRotations = [
-  Beam.PrismRotation.Off,
-  Beam.PrismRotation.Slow,
-  Beam.PrismRotation.Fast
-];
+// const testPrismRotations = [
+//   Beam.PrismRotation.Off,
+//   Beam.PrismRotation.Slow,
+//   Beam.PrismRotation.Fast
+// ];
   
-const testTilts = [
-  40,
-  50,
-  60,
-  70,
-  // 88,
-  // 100
-];
+// const testTilts = [
+//   40,
+//   50,
+//   60,
+//   70,
+//   // 88,
+//   // 100
+// ];
       
 const artnet = new ArtNet();
 
@@ -106,6 +114,8 @@ artnet.configureUniverse(westUniverseInfo);
 
 let channelData = [];
 
+let defaultLamp = Beam.Lamp.On;
+
 function setDefaultChannelData() {
   channelData[Beam.BeamChannel.ColorWheel] = Beam.Color.White
   channelData[Beam.BeamChannel.Strobe] = Beam.Strobe.Open;
@@ -122,23 +132,23 @@ function setDefaultChannelData() {
   channelData[Beam.BeamChannel.TiltFine] = 0;
   channelData[Beam.BeamChannel.Macro] = Beam.Unused;
   channelData[Beam.BeamChannel.Reset] = Beam.Reset.None;
-  channelData[Beam.BeamChannel.Lamp] = Beam.Lamp.On;
+  channelData[Beam.BeamChannel.Lamp] = defaultLamp;
 }
-setDefaultChannelData();
+
 
 //  ====== Pan =====
 
 let panStart = 0;
-let panStop = 180;
+let panStop = 160;
 let panInterval = 1;
 let panValue = panStart;
 
-function setPans(newPanStart = 0, newPanStop = 160, newPanInterval = 1) {
+function setPans(newPanStart = 0, newPanStop = 170, newPanInterval = 1) {
   panStart = newPanStart;
   panStop = newPanStop;
   panInterval = newPanInterval;
   panValue = panStart;
-  channelData[Beam.BeamChannel.Pan] = panStart;
+  channelData[Beam.BeamChannel.Pan] = panValue;
 }
 
 function nextPan() {
@@ -148,18 +158,23 @@ function nextPan() {
     panValue = panStart;
     reset = true;
   }
+  if (panValue == 0) {
+    stepInterval = 2000;
+  } else {
+    stepInterval = 125;
+  }
   channelData[Beam.BeamChannel.Pan] = panValue;
   return reset;
 }
 
 //  ====== Tilt =====
 
-let tiltStart = 40;
-let tiltStop = 80;
-let tiltInterval = 12;
+let tiltStart = 38;
+let tiltStop = 58;
+let tiltInterval = 10;
 let tiltIndex = tiltStart;
 
-function setTilts(newTiltStart = 40, newTiltStop = 80, newTiltInterval = 10) {
+function setTilts(newTiltStart = 38, newTiltStop = 58, newTiltInterval = 10) {
   tiltStart = newTiltStart;
   tiltStop = newTiltStop;
   tiltInterval = newTiltInterval;
@@ -170,7 +185,7 @@ function setTilts(newTiltStart = 40, newTiltStop = 80, newTiltInterval = 10) {
 function nextTilt() {
   let reset = false;
   tiltIndex += tiltInterval;
-  if (tiltIndex >= tiltStop) {
+  if (tiltIndex > tiltStop) {
     tiltIndex = tiltStart;
     reset = true;
   }
@@ -186,7 +201,7 @@ let colorIndex = tiltStart;
 function setColors(newcolors) {
   currentColors = newcolors;
   colorIndex = 0;
-  channelData[Beam.BeamChannel.ColorWheel] = colorIndex;
+  channelData[Beam.BeamChannel.ColorWheel] = currentColors[colorIndex];
 }
 
 function nextColor() {
@@ -203,22 +218,31 @@ function nextColor() {
 
 let sceneIndex = 0;
 
+setTilts();
+
 function nextScene() {
   if (++sceneIndex > 0) sceneIndex = 0;
 
   setScene();
 }
 
+let stepCounter = 0;
+
 function setScene() {
   setDefaultChannelData();
 
   switch (sceneIndex)
   {
-    case 0:
-      setPans(0, 180, 2);
+    case 1:
+      setPans();
       setColors(halloweenColors);
-      setTilts();
       break;
+
+    case 0:
+      setPans(0, 160);
+      setTilts(25, 36, 4);
+      setColors(darkHalloweenColors);
+    break;
 
     // case 2:
     //   // gobo
@@ -249,13 +273,18 @@ function setScene() {
     //   break;
   }
 
-  sendChannelData();
 }
+
+let stepInterval = 1000;
 
 function nextStep() {
 
   switch (sceneIndex)
   {
+    case 100:
+      if (++stepCounter > 10)
+        process.exit(0);
+      break;
     case 0:
       if (nextPan()) {
         nextTilt();
@@ -297,11 +326,13 @@ function nextStep() {
   }
 
   sendChannelData();
+
+  setTimeout(nextStep, stepInterval);
 }
   
 function sendChannelData()
 {
-  console.log("--- BeamTest::",
+  console.log("--", Date.now()/1000,
     " test=", sceneIndex,
     " c=", channelData[Beam.BeamChannel.ColorWheel],
     " p=", channelData[Beam.BeamChannel.Pan],
@@ -310,7 +341,8 @@ function sendChannelData()
     " g=", channelData[Beam.BeamChannel.Gobo],
     " pz=", channelData[Beam.BeamChannel.Prism],
     " pr=", channelData[Beam.BeamChannel.PrismRotation],
-    " l=", channelData[Beam.BeamChannel.Lamp]);
+    " l=", channelData[Beam.BeamChannel.Lamp],
+    " -- to=", stepInterval);
 
   for (var beamIndex = 0; beamIndex < beams.length; beamIndex++) {
     artnet.setChannelData(beams[beamIndex].address, beams[beamIndex].universe, beams[beamIndex].channel, channelData);
@@ -322,20 +354,18 @@ function sendChannelData()
 for (let j = 0; j < process.argv.length; j++) {
   console.log(j + ':' + (process.argv[j]));
 
+  channelData[Beam.BeamChannel.Lamp] = Beam.Lamp.On;
   if (process.argv[j] == "off") {
-    setDefaultChannelData();
-    channelData[Beam.BeamChannel.Lamp] = Beam.Lamp.Off;
-    sendChannelData();
-    process.exit(0);
+    defaultLamp = Beam.Lamp.Off;
+    sceneIndex = 100;
+    stepCounter = 0;
   }
 }
+setDefaultChannelData();
 
 setScene();
 
-let stepInterval = 20;
-
-setInterval(nextStep, stepInterval);
-
+setTimeout(nextStep, stepInterval);
 
 // const beamTestData = [
 //   { channel: 1, channelData: [
